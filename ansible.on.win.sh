f8b6316e29f2d5b66b6383710423e0a5e82b25b6
Gerald Luzangi <gerald.luzangi@gmail.com>
2016-11-07 21:11:59 -0500

    refactor ansible-on-win

--- ansible.on.win.sh
+++ ansible.on.win.sh
@@ -2,43 +2,14 @@
 
 #########
 #
-#  ANSIBLE ON WINDOWS INSTALL STEPS
+#  ANSIBLE ON WINDOWS SCRIPT TASKS:-
 #
-#  STEP 1: Add Ansible Building Tools
+#  STEP 1: Check Out A Fresh Copy Of winAnsible.git
+#  STEP 2: $HOME/ansible-on-win.sh
+#  STEP 3: Add Ansible Building Tools
+#  STEP 4: Install Python-pip
+#  STEP 5: Install Ansible Using Pip
 #
-#+ pacman -S base-devel libffi libffi-devel pkg-config gmp gmp-devel openssh openssl openssl-devel git python2 python2-setuptools
-
-##
-# Read Instruction Here : https://pip.pypa.io/en/stable/installing/
-# pip vs setuptools : https://packaging.python.org/pip_easy_install/#pip-vs-easy-install
-##
-
-#
-#  STEP 2:
-# 	+ i] Install Python-pip
-#
-#+ mkdir /usr/local/src/ ; cd /usr/local/src/ ; wget https://bootstrap.pypa.io/get-pip.py
-#+ python get-pip.py
-#
-# 	+ ii] Fix pyconfig.h FreeBSD library visibility From 1 -> 0
-#
-# /usr/include/python2.7/pyconfig.h:1222:0: warning: "__BSD_VISIBLE" redefined
-# #define __BSD_VISIBLE 1
-#
-# /* Define on FreeBSD to activate all library features */
-#+ #define __BSD_VISIBLE 0
-#
-#  STEP 3: Install Ansible Using Pip
-#
-#+ pip install -U -r $HOME/devops/ansible.pip.requirements.txt
-#
-#:: There are 54 members in group base-devel:
-#:: Repository msys
-#   1) asciidoc  2) autoconf  3) autoconf2.13  4) autogen  5) automake-wrapper  6) automake1.10  7) automake1.11  8) automake1.12  9) automake1.13  10) automake1.14  11) automake1.15
-#   12) automake1.6  13) automake1.7  14) automake1.8  15) automake1.9  16) bison  17) diffstat  18) diffutils  19) dos2unix  20) file  21) flex  22) gawk  23) gdb  24) gettext
-#   25) gettext-devel  26) gperf  27) grep  28) groff  29) help2man  30) intltool  31) lemon  32) libtool  33) libunrar  34) m4  35) make  36) man-db  37) pacman  38) pactoys-git  39) patch
-#   40) patchutils  41) perl  42) pkg-config  43) pkgfile  44) quilt  45) rcs  46) scons  47) sed  48) swig  49) texinfo  50) texinfo-tex  51) ttyrec  52) unrar  53) wget  54) xmlto
-
 #######
 
 ANSIBLE_DIR=$HOME/.ansible
@@ -56,21 +27,22 @@ if [ -f /etc/ansible-on-win.installed ]
     #source ./hacking/env-setup
     cd $CURRENT_DIR
 
+    echo " "
     echo "Ansible-On-Windows Upgrade Completed.\n"
     echo "Remember to setup the ssh-agent.\n"
     echo "Please restart MSYS-SHELL. \n"
   else
     ##
-    # Install Ansible On Windows Packages To Resolve Library Dependencies
-    ##
-    pacman -Sy --noconfirm pacman
-    pacman -Syu --noconfirm base-devel curl libffi libffi-devel gmp gmp-devel openssh openssl openssl-devel git python2 python2-setuptools
-
-    ##
     # Fetch Ansible On Windows Related Fixes From The Repo
     ##
     git clone https://gitlab.com/gluzangi/winansible.git $HOME/
     cp $HOME/ansible.playbook/roles/box/files/pyconfig.h /usr/include/python2.7/pyconfig.h
+
+    ##
+    # Prepare Ansible On Windows Building Tools
+    ##
+    pacman -Sy --noconfirm pacman
+    pacman -Syu --noconfirm base-devel curl libffi libffi-devel gmp gmp-devel openssh openssl openssl-devel git python2 python2-setuptools
 
     ###
     # Install Python PIP
@@ -78,7 +50,7 @@ if [ -f /etc/ansible-on-win.installed ]
     curl https://bootstrap.pypa.io/get-pip.py | python
 
     ###
-    # Use PIP to install Ansible And Automatically Resolve Dependencies
+    # Use PIP To Install Ansible And Automatically Resolve Dependencies
     ###
     pip install -U -r $HOME/ansible.pip.requirements.txt
     ansible --version
